9dfaa596631851738430912b7375047aa26ba263
lhc_programmer <80775460+lihaichao110@users.noreply.github.com>
2025-01-21 17:15:57 +0800

    Update updateVersion.mjs

--- scripts/updateVersion.mjs
+++ scripts/updateVersion.mjs
@@ -17,39 +17,7 @@ function updateVersion() {
     const packagePath = path.resolve('./package.json');
     const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 
-    let parts = packageJson.version?.split('-');
-    // 主版本号
-    let mainParts = parts[0].split('.');
-    // 测试版本号
-    let prereleaseParts = parts[1]? parts[1]?.split('.') : [];
-
-    // 判断是不是测试版本，版本号存不存在 -
-    switch (versionTypeValue) {
-      case versionType.beta:
-        let prereleaseVersion = parseInt(prereleaseParts?.[prereleaseParts.length - 1] || 0)
-        ++prereleaseVersion
-        packageJson.version = `${mainParts.join('.')}-${versionType.beta}.${prereleaseVersion}`;
-        break;
-      case versionType.patch:
-        mainParts[2] = mainParts[2] ? ++mainParts[2] : 1
-        packageJson.version = mainParts.join('.')
-        console.log(mainParts,  packageJson.version , 'mainParts');
-        break
-
-      case versionType.minor:
-        mainParts[1] = mainParts[1] ? ++mainParts[1] : 1
-        packageJson.version = mainParts.join('.')
-        break
-
-      case versionType.major:
-        mainParts[0] = mainParts[0] ? ++mainParts[0] : 1
-        packageJson.version = mainParts.join('.')
-        break
-
-      default:
-        packageJson.version = packageJson.version
-        break;
-    }
+    packageJson.version = '1.1.1'
     fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2))
   } catch (error) {
     process.exit(1);
