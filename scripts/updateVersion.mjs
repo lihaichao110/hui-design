5353d73137b76ac7f2f54c7fd5330b223ea74418
lhc_programmer <80775460+lihaichao110@users.noreply.github.com>
2025-01-22 17:02:01 +0800

    更新版本

--- scripts/updateVersion.mjs
+++ scripts/updateVersion.mjs
@@ -17,7 +17,7 @@ function updateVersion() {
     const packagePath = path.resolve('./package.json');
     const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 
-    packageJson.version = '1.1.88'
+    packageJson.version = '9.9.99'
     fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2))
   } catch (error) {
     process.exit(1);
